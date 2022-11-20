c95712bf24b61da091659a31679a068deb7efbb6
aelvismorales <faviotio123@gmail.com>
2022-11-20 12:57:51 -0500

    Modificando navbar para redireccionar entre secciones

--- src/componentes/Nav/Nav.js
+++ src/componentes/Nav/Nav.js
@@ -1,31 +1,36 @@
 import "./Nav.css"
 import hamburguer from "../../assets/hamburguer.jpg"
-
+import Main from "../Main/Main";
+import Nosotros from "../Nosotros/Nosotros";
+import Equipo from "../Equipo/Equipo";
+import Testimonios from "../Testimonios/Testimonios";
+import Footer from "../Footer/Footer";
+import CallToAction from "../CallToAction/CallToAction";
 function Navbar() {
   return (
-    <nav className="nav">
+    <><nav className="nav">
       <ul className="nav-list">
         <li>
-          <a href="#"> LetSkole</a>
+          <a href="#main"> LetSkole</a>
         </li>
         <li>
-          <a href="#">Nosotros</a>
+          <a href="#nosotros">Nosotros</a>
         </li>
         <li>
-          <a href="#">Miembros</a>
+          <a href="#equipo">Miembros</a>
         </li>
         <li>
-          <a href="#">Testimonios</a>
+          <a href="#testimonios">Testimonios</a>
         </li>
         <li>
-          <a href="#">Contactanos</a>
+          <a href="#calltoaction">Contactanos</a>
         </li>
         <li>
-          <a href="#">Redes Sociales</a>
+          <a href="#footer">Redes Sociales</a>
         </li>
       </ul>
       <img id="hamburguer" src={hamburguer} />
-    </nav>
+    </nav><section id="main"><Main /></section><section id="nosotros"> <Nosotros /></section><section id="equipo"><Equipo /></section><section id="testimonios"><Testimonios /></section><section id="calltoaction"><CallToAction /></section><section id="footer"><Footer /></section></>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./componentes/Nav/Nav";
-import Main from "./componentes/Main/Main";
-import Nosotros from "./componentes/Nosotros/Nosotros";
-import Equipo from "./componentes/Equipo/Equipo";
-import Testimonios from "./componentes/Testimonios/Testimonios";
-import Footer from "./componentes/Footer/Footer";
 import "./index.css";
-import CallToAction from "./componentes/CallToAction/CallToAction";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
@@ -15,11 +9,5 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <Fragment className="main-card">
     <Navbar />
-    <Main/>
-    <Nosotros/>
-    <Equipo/>
-    <Testimonios/>
-    <CallToAction />
-    <Footer />
   </Fragment>
 );
